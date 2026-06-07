e20c3ba5051931539948c49be33d54623320e254
JairoOrtiz06 <os24002@ues.edu.sv>
2026-06-06 20:42:45 -0600

    agregando json con la base de datos

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,15 @@
 <script>
     import Login from './paginas/Login.svelte';
+
+    import { dbPromise } from './base_datos/database.js';
+
+    async function probarBD() {
+        const db = await dbPromise;
+        console.log("Base de datos creada:", db);
+    }
+
+    probarBD();
 </script>
+
 
 <Login />
